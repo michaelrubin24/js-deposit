a5d7c70a43503df3a57f284a639282dbab28ee65
Michael Rubinshtein <deliverypoint7@yandex.ru>
2017-10-24 14:49:15 +0300

    edited text in variable result

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,6 @@ var address_flat = prompt("Квартира:","не указано");
 var sum = +prompt("Сумма которую вы хотите вложить:","не указано");
 var procent = +prompt("Процент под который вы хотите вложить деньги:","не указано");
 var months = +prompt("На какой срок вы хотите положить свои деньги (кол-во месяцев):","не указано");
-var vklad = (sum*procent)/100*months+sum;
-var result = alert("Здравствуйте, на номер телефона "+telephone+",\n"+"На адрес: улица "+address_street+", дом "+address_house+", квартира "+address_flat+"\n"+"Зарегистрирован счёт на сумму: "+sum+", процент "+procent+",cрок "+months+" месяцев"+"\n"+"После "+months+ " месяцев вы сможете забрать вклад который = "+vklad+"$");
+var deposit = (sum*procent)/100*months+sum;
+var result = alert("Здравствуйте, на номер телефона "+telephone+",\n"+"На адрес: улица "+address_street+", дом "+address_house+", квартира "+address_flat+"\n"+"Зарегистрирован счёт на сумму: "+sum+", процент "+procent+",cрок "+months+" месяцев"+"\n"+"После "+months+ " месяцев вы сможете забрать вклад который = "+deposit+"$");
  
